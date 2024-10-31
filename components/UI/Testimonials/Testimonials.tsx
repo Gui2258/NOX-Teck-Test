0cc5b521f4c77e79f18adf9280c3c136fb00aaf1
Guido Morffi <guidotele@gmail.com>
2024-10-30 22:08:10 -0400

    style(testimonials) aded svg background on left

--- components/UI/Testimonials/Testimonials.tsx
+++ components/UI/Testimonials/Testimonials.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { DecoBottom } from '@/icons/DecoBottom';
-import { DecoTop } from '@/icons/DecoTop';
 import { TestimonialCard } from './TestimonialCard';
-import { DecoRIgthBorder } from '@/icons/DecoRIgthBorder';
+import { DecoRigthBorder } from '@/icons/DecoRIgthBorder';
 import { DecoRigthSolid } from '@/icons/DecoRigthSolid';
+import { DecoLeftSolid } from '@/icons/DecoLeftSolid';
+import { DecoLeftBorder } from '@/icons/DecoLeftBorder';
 
 export const Testimonials = () => {
     return (
         <div id="test" className="relative gap-[32px] px-[16px] py-[48px]  w-full bg-[#FDF4FF]">
             <div className="absolute right-0 top-0 z-10">
-                <DecoRIgthBorder />
+                <DecoRigthBorder />
             </div>
             <div className="absolute right-0 top-0">
                 <DecoRigthSolid />
             </div>
-            {/* <div className="absolute bottom-0 left-0">
-                <DecoBottom />
-            </div> */}
+            <div className="absolute bottom-0 left-0">
+                <DecoLeftSolid />
+            </div>
+            <div className="absolute bottom-0 left-0">
+                <DecoLeftBorder />
+            </div>
 
             {/* Contenido del componente */}
             <div className="relative z-10 gap-[32px] flex w-full flex-col items-center justify-center">
