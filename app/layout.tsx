0d72f1dae20fe382ff02b30229d9dfde3a4357a1
Guido <guidotele@gmail.com>
2024-11-02 18:01:38 -0400

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { Roboto } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = localFont({
     src: './fonts/GeistVF.woff',
@@ -36,6 +37,7 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${roboto.className} bg-white text-black ${geistMono.variable} antialiased`}
             >
                 {children}
+                <Analytics />
             </body>
         </html>
     );
