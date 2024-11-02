6dd7e7432a453899331036035e3b37fcb90c425c
Guido <guidotele@gmail.com>
2024-11-02 18:18:20 -0400

    Ajustes de fuente en el fotter

--- components/UI/Fotter/FotterItem.tsx
+++ components/UI/Fotter/FotterItem.tsx
@@ -9,12 +9,12 @@ export const FotterItem: React.FC<Props> = ({ title, links }) => {
     return (
         <div>
             <div className='py-[12px] gap-[8px]'>
-                <p className='font-medium text-[16px] text-[#475569]'>{title}</p>
+                <p className='font-bold text-[16px] text-[#475569]'>{title}</p>
             </div>
 
             {links.map((link, index) => (
                 <div key={index} className='py-[12px] gap-[8px]'>
-                    <p className='text-[16px] text-[#475569]'>{link}</p>
+                    <p className='text-[16px] font-normal text-[#475569]'>{link}</p>
                 </div>
             ))}
         </div>
